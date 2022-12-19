5ac7aac37cc3ad0c847bcd72e36217b9ae56f9f4
Jonathan Sivahera <jsivahera@icloud.com>
2022-12-19 23:46:54 +0200

    Scaffold resources for comics

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_17_235130) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_213836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comics", force: :cascade do |t|
+    t.boolean "favorite"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
