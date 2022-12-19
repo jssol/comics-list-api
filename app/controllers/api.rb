7044a6cc749a1de6450349f8489bf19279b091f2
Jonathan Sivahera <jsivahera@icloud.com>
2022-12-19 23:30:48 +0200

    Create encrypted hash for API keys

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -1,13 +1,14 @@
 require 'dotenv'
-require 'md5'
+require 'digest'
 Dotenv.load
 
 def get_url(endpoint)
   base_url = "https://gateway.marvel.com/v1/public"
   public_key = ENV["MARVEL_PUBLIC_KEY"]
   private_key = ENV["MARVEL_PRIVATE_KEY"]
+  md5 = Digest::MD5.new
   ts = Time.now.to_i
-  hash = MD5.new.digest "#{ts}#{private_key}#{public_key}"
+  hash = md5.digest "#{ts}#{private_key}#{public_key}"
 
   "#{base_url}#{endpoint}?ts=#{ts}&apikey=#{public_key}&hash=#{hash}"
 end
