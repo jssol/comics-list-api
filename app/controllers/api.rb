e3be8934b779813acb863491a3959e08dae16836
Jonathan Sivahera <jsivahera@icloud.com>
2022-12-19 23:32:57 +0200

    Fix linters

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -3,9 +3,9 @@ require 'digest'
 Dotenv.load
 
 def get_url(endpoint)
-  base_url = "https://gateway.marvel.com/v1/public"
-  public_key = ENV["MARVEL_PUBLIC_KEY"]
-  private_key = ENV["MARVEL_PRIVATE_KEY"]
+  base_url = 'https://gateway.marvel.com/v1/public'
+  public_key = ENV.fetch('MARVEL_PUBLIC_KEY', nil)
+  private_key = ENV.fetch('MARVEL_PRIVATE_KEY', nil)
   md5 = Digest::MD5.new
   ts = Time.now.to_i
   hash = md5.digest "#{ts}#{private_key}#{public_key}"
