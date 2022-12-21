3e150604dcae6d55a597115160d6e87e9e85a758
Jonathan Sivahera <jsivahera@icloud.com>
2022-12-21 20:14:17 +0200

    Encode using hexdigest

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -1,14 +1,15 @@
 require 'dotenv'
+require 'json'
 require 'digest'
 Dotenv.load
 
 def get_url(endpoint)
-  base_url = 'https://gateway.marvel.com'
+  base_url = 'https://gateway.marvel.com/v1/public'
   public_key = ENV.fetch('MARVEL_PUBLIC_KEY', nil)
   private_key = ENV.fetch('MARVEL_PRIVATE_KEY', nil)
   md5 = Digest::MD5.new
   ts = Time.now.to_i
-  hash = md5.digest "#{ts}#{private_key}#{public_key}"
+  hash = md5.hexdigest "#{ts}#{private_key}#{public_key}"
 
   "#{base_url}#{endpoint}?ts=#{ts}&apikey=#{public_key}&hash=#{hash}"
 end
