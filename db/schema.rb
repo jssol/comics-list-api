df507cdcc54051403fb316be6a359daac4ab2dfd
Jonathan Sivahera <jsivahera@icloud.com>
2022-12-21 16:39:09 +0200

    Scaffold events

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_213836) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_21_143742) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comics", force: :cascade do |t|
     t.boolean "favorite"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
