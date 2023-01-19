99abe4efa41c5aa43369ffc272d3269ca9d40963
Jonathan Sivahera <jsivahera@icloud.com>
2023-01-19 08:57:57 +0200

    Fix linters

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -3,24 +3,24 @@ require 'json'
 require 'digest'
 Dotenv.load
 
-$base_url = 'https://gateway.marvel.com/v1/public'
-
 def get_url(endpoint)
+  base_url = 'https://gateway.marvel.com/v1/public'
   public_key = ENV.fetch('MARVEL_PUBLIC_KEY', nil)
   private_key = ENV.fetch('MARVEL_PRIVATE_KEY', nil)
   md5 = Digest::MD5.new
   ts = Time.now.to_i
   hash = md5.hexdigest "#{ts}#{private_key}#{public_key}"
 
-  "#{$base_url}#{endpoint}?ts=#{ts}&apikey=#{public_key}&hash=#{hash}"
+  "#{base_url}#{endpoint}?ts=#{ts}&apikey=#{public_key}&hash=#{hash}"
 end
 
 def get_search_url(type, query)
+  base_url = 'https://gateway.marvel.com/v1/public'
   public_key = ENV.fetch('MARVEL_PUBLIC_KEY', nil)
   private_key = ENV.fetch('MARVEL_PRIVATE_KEY', nil)
   md5 = Digest::MD5.new
   ts = Time.now.to_i
   hash = md5.hexdigest "#{ts}#{private_key}#{public_key}"
 
-  "#{$base_url}/#{type}?#{query}&ts=#{ts}&apikey=#{public_key}&hash=#{hash}"
+  "#{base_url}/#{type}?#{query}&ts=#{ts}&apikey=#{public_key}&hash=#{hash}"
 end
