22b5bc635c905f793a6d1aa319262c8a66a6679c
Jonathan Sivahera <jsivahera@icloud.com>
2023-01-25 19:23:23 +0200

    Add a UrlHelperMethods

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'capybara/rspec'
-require_relative 'url_helper_methods'
+require './url_helper_methods'
 
 Capybara.javascript_driver = :selenium_chrome
 Capybara.app_host = 'http://localhost:3001'
