cbe1f87e0fb2733ff29f6262a44c18b60df4cab1
Jonathan Sivahera <jsivahera@icloud.com>
2023-01-29 16:07:18 +0200

    Fix linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,50 +3,53 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.all
+    users = User.all
 
-    render json: @users
+    render json: users
   end
 
   # GET /users/1
   def show
-    render json: @user
+    render json: user
   end
 
   # POST /users
   def create
-    @user = User.new(user_params)
+    user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if user.save
+      token = issue_token(user)
+      render json: { user: UserSerializer.new(user), jwt: token }, status: :created
+    elsif user.errors.messages
+      render json: { error: user.errors.messages }, status: :error
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { error: 'User could not be created. Please try again.' }, status: :error
     end
   end
 
   # PATCH/PUT /users/1
   def update
-    if @user.update(user_params)
-      render json: @user
+    if user.update(user_params)
+      render json: user
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: user.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /users/1
   def destroy
-    @user.destroy
+    user.destroy
   end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_user
-    @user = User.find(params[:id])
+    User.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:name, :username, :email)
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
   end
 end
