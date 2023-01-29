82244ae07580dcb7eba1ab40e0fe14a679a7840b
Jonathan Sivahera <jsivahera@icloud.com>
2023-01-29 15:17:49 +0200

    Add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :events, dependent: :destroy
+  has_many :comics, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, { format: { with: URI::MailTo::EMAIL_REGEXP }, message: "This email already exists" }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
