07033ec00d8118ce41228e3b1be4af06a8cc4531
Jonathan Sivahera <jsivahera@icloud.com>
2023-01-29 15:03:20 +0200

    Add password to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_29_124949) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_29_130253) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_29_124949) do
     t.string "role", default: "reader"
     t.integer "followers_count", default: 0
     t.integer "followed_count", default: 0
+    t.string "password_digest"
     t.index ["first_name"], name: "index_users_on_first_name"
     t.index ["last_name"], name: "index_users_on_last_name"
   end
