fd300190b2203698206b6a89ec3ca8a45090e707
Jonathan Sivahera <jsivahera@icloud.com>
2023-01-30 00:01:47 +0200

    Add temp faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+User.create!(username: "solotov", email: "sivaheradev@gmail.com", first_name: "Jonathan", last_name: "Sivahera", role: "admin", passwo
+rd: "J0nathan8M@velmar")
