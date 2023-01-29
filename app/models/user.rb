8c0a586d612af9d91a6458efce2493bcd39b3d88
Jonathan Sivahera <jsivahera@icloud.com>
2023-01-29 17:05:40 +0200

    Change username uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :events, dependent: :destroy
   has_many :comics, dependent: :destroy
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: { message: 'This username is already in use'}
   validates :email, presence: true, uniqueness: { message: 'This email is already in use' }
   validates :first_name, presence: true
   validates :last_name, presence: true
