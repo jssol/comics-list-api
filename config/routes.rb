9e6c8b3a74ab3bb383a0d94d486740f059438346
Jonathan Sivahera <jsivahera@icloud.com>
2023-01-29 16:20:15 +0200

    Add custom auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :events
-  resources :comics
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :events
+  resources :comics
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  get "/authorized", to: "sessions#show"
+  get "/dashboard", to: "users#show"
 
   get '/search', to: 'comics#search'
 end
